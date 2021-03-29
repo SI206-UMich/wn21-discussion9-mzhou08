f0df9484d2c45b1432e6e7e06a242893e6f7a692
Michael Zhou <mzhou08@gmail.com>
2021-03-29 16:53:22 -0400

    getCharmanderLink done

--- diss_9.py
+++ diss_9.py
@@ -6,11 +6,17 @@ import unittest
 # Task 1: Get the URL that links to the Pokemon Charmander's webpage.
 # HINT: You will have to add https://pokemondb.net to the URL retrieved using BeautifulSoup
 def getCharmanderLink(soup):
-    pass
+    url = 'https://pokemondb.net'
+    tags = soup.find_all('a', class_ = 'ent-name')
+    for tag in tags:
+        if tag.text == 'Charmander':
+            ext = tag['href']
+
+    return url + ext
 
 # Task 2: Get the details from the box below "Egg moves". Get all the move names and store
 #         them into a list. The function should return that list of moves.
-def getEggMoves(pokemon):
+'''def getEggMoves(pokemon):
     url = 'https://pokemondb.net/pokedex/'+pokemon
     #add code here
 
@@ -33,7 +39,7 @@ def findLetters(sentences):
 
 
     #return the list of the last letter of all words that begin or end with a capital letter
-
+'''
 
 
 def main():
@@ -41,7 +47,7 @@ def main():
     r = requests.get(url)
     soup = BeautifulSoup(r.text, 'html.parser')
     getCharmanderLink(soup)
-    getEggMoves('scizor')
+    #getEggMoves('scizor')
 
 class TestAllMethods(unittest.TestCase):
     def setUp(self):
@@ -49,14 +55,14 @@ class TestAllMethods(unittest.TestCase):
 
     def test_link_Charmander(self):
         self.assertEqual(getCharmanderLink(self.soup), 'https://pokemondb.net/pokedex/charmander')
-
+'''
     def test_egg_moves(self):
         self.assertEqual(getEggMoves('scizor'), ['Counter', 'Defog', 'Feint', 'Night Slash', 'Quick Guard'])
 
     def test_findLetters(self):
         self.assertEqual(findLetters(['Come eat lunch at 12','there"s a party @2pm', 'practice @7am','nothing']), ['2pm', '7am'])
         self.assertEqual(findLetters(['There is show @12pm if you want to join','I will be there @ 2pm', 'come at @3 pm will be better']), ['12pm', '3 pm'])
-
+'''
 if __name__ == "__main__":
     main()
-    unittest.main(verbosity = 2)
+    unittest.main(exit = False, verbosity = 2)
