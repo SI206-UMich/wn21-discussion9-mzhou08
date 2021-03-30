3acd71df79e6de08a2c692276a8e05e034344292
Michael Zhou <mzhou08@gmail.com>
2021-03-29 22:32:22 -0400

    egg move done

--- diss_9.py
+++ diss_9.py
@@ -16,21 +16,37 @@ def getCharmanderLink(soup):
 
 # Task 2: Get the details from the box below "Egg moves". Get all the move names and store
 #         them into a list. The function should return that list of moves.
-'''def getEggMoves(pokemon):
-    url = 'https://pokemondb.net/pokedex/'+pokemon
+def getEggMoves(pokemon):
+    url = 'https://pokemondb.net/pokedex/' + pokemon
     #add code here
+    r = requests.get(url)
+    soup = BeautifulSoup(r.text, 'html.parser')
+    head = soup.find('h3', text = 'Egg moves')
+    table = head.findNext('table', class_ = 'data-table')
+    cells = table.find_all('td', class_ = 'cell-name')
+    moves = [cell.text for cell in cells]
+    '''
+    discarded code:
+    tried to go onto egg moves page to get stuff, but found out about find_next_siblings
 
+    links = soup.find_all('em')
+    for link in links:
+        if link.text.lower() == pokemon.lower():
+            l = link.find('a')['href']'''
+    return moves
 # Task 3: Create a regex expression that will find all the times that have these formats: @2pm @5 pm @10am
 # Return a list of these times without the '@' symbol. E.g. ['2pm', '5 pm', '10am']
+'''
 def findLetters(sentences):
     # initialize an empty list
-    
+    ls = []
 
     # define the regular expression
-    
+    reg = r'\b@(?:{0-9}|10)\w?(?:am|pm)'
 
     # loop through each sentence or phrase in sentences
-    
+    for sentence in sentences:
+        
 
     # find all the words that match the regular expression in each sentence
        
@@ -40,7 +56,6 @@ def findLetters(sentences):
 
     #return the list of the last letter of all words that begin or end with a capital letter
 '''
-
 
 def main():
     url = 'https://pokemondb.net/pokedex/national'
@@ -55,10 +70,10 @@ class TestAllMethods(unittest.TestCase):
 
     def test_link_Charmander(self):
         self.assertEqual(getCharmanderLink(self.soup), 'https://pokemondb.net/pokedex/charmander')
-'''
+
     def test_egg_moves(self):
         self.assertEqual(getEggMoves('scizor'), ['Counter', 'Defog', 'Feint', 'Night Slash', 'Quick Guard'])
-
+'''
     def test_findLetters(self):
         self.assertEqual(findLetters(['Come eat lunch at 12','there"s a party @2pm', 'practice @7am','nothing']), ['2pm', '7am'])
         self.assertEqual(findLetters(['There is show @12pm if you want to join','I will be there @ 2pm', 'come at @3 pm will be better']), ['12pm', '3 pm'])
